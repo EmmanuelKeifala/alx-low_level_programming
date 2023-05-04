e919cb90a3cb66f9357f87a7baef3dfc9120e9df
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2023-05-04 08:31:22 -0700

    get_bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,6 +10,12 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	return ((n >> index) & 1);
-}
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
 
+	unsigned long int mask = 1UL << index;
+
+	return ((n & mask) != 0);
+}
