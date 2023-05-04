400a509ce36f3862c9c3cc477a0375af0b02184d
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2023-05-04 07:47:53 -0700

    print_binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,6 +3,7 @@
  * print_binary - Prints the binary representation of an unsigned long int.
  *
  * @n_lng: The unsigned long int to convert to binary and print.
+ *  * Return: returns nothing.
  */
 
 void print_binary(unsigned long int n_lng)
