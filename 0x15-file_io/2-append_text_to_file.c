7ec3e12dd44cab0f3d1b934c3f467289c248e938
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2023-05-09 07:44:58 -0700

    append_text_to_file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int opn, n_write, len = 0;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -23,13 +23,13 @@ int append_text_to_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	opn = open(filename, O_WRONLY | O_APPEND);
-	n_write = write(opn, text_content, len);
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
 
-	if (opn == -1 || n_write == -1)
+	if (o == -1 || w == -1)
 		return (-1);
 
-	close(opn);
+	close(o);
 
 	return (1);
 }
