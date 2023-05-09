a614d1d5feb1ef6d277006f6753ed3ecb498a455
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2023-05-09 08:16:15 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,28 +1,69 @@
+#include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include <errno.h>
 
 #define BUFFER_SIZE 1024
 
 /**
- * print_error - print error message and exit program with error code
- * @error_code: the exit code to use
- * @message: the error message to print
- * @file_name: the name of the file to use in the error message
- * @fd: the file descriptor to close, or -1 if no file descriptor to close
+ * print_error - prints an error message to stderr
+ * @error_code: error code to exit with
+ * @message: printf-style format string for error message
+ * @filename: name of file to include in error message
+ * @fd: file descriptor to include in error message
  */
-void print_error(int error_code, char *message, char *file_name, int fd)
+void print_error(int error_code, char *message, char *filename, int fd)
 {
-	dprintf(STDERR_FILENO, message, file_name);
-	if (fd >= 0)
-	{
-		close(fd);
-	}
-	exit(error_code);
+dprintf(STDERR_FILENO, message, filename, fd);
+exit(error_code);
+}
+
+/**
+ * open_file - opens a file and handles errors
+ * @filename: name of file to open
+ * @flags: flags to use when opening file
+ * @mode: mode to use when creating file
+ * @error_code: error code to exit with if open fails
+ *
+ * Return: file descriptor on success, or -1 on failure
+ */
+int open_file(char *filename, int flags, mode_t mode, int error_code)
+{
+int fd;
+
+fd = open(filename, flags, mode);
+
+if (fd < 0)
+{
+print_error(error_code, "Error: Can't open file %s\n", filename, -1);
+}
+
+return (fd);
+}
+
+/**
+ * copy_file - copies contents of one file to another
+ * @fd_from: file descriptor of source file
+ * @fd_to: file descriptor of destination file
+ */
+void copy_file(int fd_from, int fd_to)
+{
+char buffer[BUFFER_SIZE];
+ssize_t bytes_read, bytes_written;
+
+do {
+bytes_read = read(fd_from, buffer, BUFFER_SIZE);
+if (bytes_read < 0)
+{
+print_error(98, "Error: Can't read from file %s\n", "", fd_from);
+}
+bytes_written = write(fd_to, buffer, bytes_read);
+if (bytes_written < 0)
+{
+print_error(99, "Error: Can't write to %s\n", "", fd_to);
+}
+} while (bytes_read > 0);
 }
 
 /**
@@ -34,55 +75,28 @@ void print_error(int error_code, char *message, char *file_name, int fd)
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to;
-	ssize_t bytes_read, bytes_written;
-	char buffer[BUFFER_SIZE];
+int fd_from, fd_to;
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
-		return (97);
-	}
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+return (97);
+}
 
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from < 0)
-	{
-		print_error(98, "Error: Can't read from file %s\n", argv[1], -1);
-	}
+fd_from = open_file(argv[1], O_RDONLY, 0, 98);
+fd_to = open_file(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0644, 99);
 
-	fd_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0644);
-	if (fd_to < 0)
-	{
-		print_error(99, "Error: Can't write to %s\n", argv[2], fd_from);
-	}
+copy_file(fd_from, fd_to);
 
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written < 0)
-		{
-			print_error(99, "Error: Can't write to %s\n", argv[2], fd_from);
-		}
-		if (bytes_written != bytes_read)
-		{
-			print_error(99, "Error: Incomplete write to %s\n", argv[2], fd_from);
-		}
-	}
-	if (bytes_read < 0)
-	{
-		print_error(98, "Error: Can't read from file %s\n", argv[1], fd_to);
-	}
+if (close(fd_from) < 0)
+{
+print_error(100, "Error: Can't close fd %d\n", "", fd_from);
+}
+if (close(fd_to) < 0)
+{
+print_error(100, "Error: Can't close fd %d\n", "", fd_to);
+}
 
-	if (close(fd_from) < 0)
-	{
-		print_error(100, "Error: Can't close fd %d\n", "", fd_from);
-	}
-
-	if (close(fd_to) < 0)
-	{
-		print_error(100, "Error: Can't close fd %d\n", "", fd_to);
-	}
-
-	return (0);
+return (0);
 }
 
