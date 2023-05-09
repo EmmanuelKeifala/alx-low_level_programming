4b7e87c87b8689bd0720cb7cc5a706efe3bd5b6c
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2023-05-09 09:01:32 -0700

    files

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,21 +9,30 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd;
+	ssize_t nrd, nwr;
 	char *buf;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
+
+	if (!filename)
+		return (0);
 
 	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
 
 	free(buf);
-	close(fd);
-	return (w);
+
+	return (nwr);
 }
 
 
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,14 +1,11 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#ifndef MAIN_H
-#define MAIN_H
-
 #include <unistd.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <stddef.h>
-
 
 
 ssize_t read_textfile(const char *filename, size_t letters);
