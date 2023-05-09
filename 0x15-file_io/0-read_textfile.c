b2992581da5ba86bef524ba5efc52dcb0e5e9ce4
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2023-05-09 07:07:08 -0700

    read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,51 +1,51 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output
- *
- * @filename: pointer to the file name
- * @letters: number of letters it should read and print
- *
- * Return: actual number of letters it could read and print, or 0 if:
- *         - file can not be opened or read
- *         - filename is NULL
- *         - write fails or does not write the expected amount of bytes
- */
+* read_textfile - this will read a certain file and output it
+* @filename: this pointing to the file to read
+* @letters: the size to read for the file
+* Return: the real size read and printed
+*/
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t n_bytes_read, n_bytes_written;
-	char *buffer;
+int fd; /* file descriptor */
+ssize_t n_bytes_read, n_bytes_written;
+char *buffer;
 
-	if (filename == NULL)
-   		 return (0);
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-    		return (0);
-	/* Open the file in read-only mode */
-	FILE *file = fopen(filename, "r");
-	if (file == NULL)
-  	{
-    		free(buffer);
-    		return (0);
-  	}
-	  /* Read from the file and print to standard output */
-	n_bytes_read = read(file, buffer, letters);
-	if (n_bytes_read == -1)
-	{
-		free(buffer);
-		close(file);
-		return (0);
-	}
-	n_bytes_written = write(STDOUT_FILENO, buffer, n_bytes_read);
-    	if (n_bytes_written == -1 || n_bytes_written != n_bytes_read)
-    	{
-        	free(buffer);
-        	close(file);
-        	return (0);
-    	}
+if (filename == NULL)
+return (0);
 
-    	free(buffer);
-    	close(file);
-    	return (n_bytes_read);
+/* open */
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
+
+/* malloc buffer */
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+return (0);
+
+/* read */
+n_bytes_read = read(fd, buffer, letters);
+if (n_bytes_read == -1)
+{
+free(buffer);
+close(fd);
+return (0);
+}
+
+/* write */
+n_bytes_written = write(STDOUT_FILENO, buffer, n_bytes_read);
+if (n_bytes_written == -1)
+{
+free(buffer);
+close(fd);
+return (0);
+}
+
+close(fd);
+return (n_bytes_read);
+
 }
 
