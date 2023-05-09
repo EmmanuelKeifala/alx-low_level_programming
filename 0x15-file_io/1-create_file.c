fb51e4a5edd04f9ea50a8fb4f804903518b5d1d2
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2023-05-09 07:33:33 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,25 +11,25 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int opn, wrt, len = 0;
+	int o, w, len = 0;
 
-if (filename == NULL)
-return (-1);
+	if (filename == NULL)
+		return (-1);
 
-if (text_content != NULL)
-{
-for (len = 0; text_content[len];)
-len++;
-}
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-opn = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-wrt = write(opn, text_content, len);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
 
-if (opn == -1 || wrt== -1)
-return (-1);
+	if (o == -1 || w == -1)
+		return (-1);
 
-close(opn);
+	close(o);
 
-return (1);
+	return (1);
 }
 
